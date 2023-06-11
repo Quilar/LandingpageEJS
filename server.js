b73cac23a58cae06ca776745858d9ad836b28d81
MomoQuilar <aajour@quilar.de>
2023-06-11 21:00:31 +0200

    ORGANIZATION: organized the main site and removed all unused partials for now. brought them over to an index-bak.ejs site that gets served at /backupsites. We now gonna distribute them further

--- server.js
+++ server.js
@@ -36,6 +36,22 @@ catch(err){
   
 });
 
+app.get('/backupsites', async (req, res) => {
+  console.log("request incoming");
+  try{
+    const news = await fs.promises.readFile(`${__dirname}/public/content/news/news.json`);
+    const testimonials = await fs.promises.readFile(`${__dirname}/public/content/testimonials/testimonies.json`);
+    
+    if(news && testimonials) res.render('pages/index-bak', {news: JSON.parse(news), testimonials: JSON.parse(testimonials)});
+    else throw Error("Ein Server Error ist aufgetreten");
+  }
+  catch(err){
+    res.status(404).render(404);
+  }
+    
+  });
+  
+
 // about page
 app.get('/about', function(req, res) {
   res.render('pages/about');
