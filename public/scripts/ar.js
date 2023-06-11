ce3637b6c42bf8db1d5dca6fcde430ae8800083f
MomoQuilar <aajour@quilar.de>
2023-06-11 19:49:55 +0200

    AR: Modified the layout so it matches with visualization sections we are about to use after each claim

--- public/scripts/ar.js
+++ public/scripts/ar.js
@@ -1,6 +1,6 @@
 const ArAnimation = ()=>{
     var frame = document.getElementById('frame');
-    var container = document.querySelector('.ar-section');
+    var container = document.querySelector('.ar-wrapper');
     var virtualImage = document.querySelector('.virtual-image');
 
     const isMobileDevice = () => {return /Mobi|Android/i.test(navigator.userAgent)};
@@ -23,15 +23,16 @@ const ArAnimation = ()=>{
 
 
         var containerTop = container.getBoundingClientRect().top;
+        var containerLeft = container.getBoundingClientRect().left;
        
         frame.style.top = y - containerTop + 'px';
-        frame.style.left = x + 'px';
+        frame.style.left = x - containerLeft + 'px';
         var frameRect = frame.getBoundingClientRect();
         var screenDelta = frameRect.width * 0.0745;
         var topY = frameRect.top - containerTop + screenDelta;
         var bottomY = frameRect.top + frameRect.height - containerTop - screenDelta;
-        var leftX = frameRect.left + screenDelta;
-        var rightX = frameRect.left + frameRect.width - screenDelta;
+        var leftX = frameRect.left - containerLeft + screenDelta;
+        var rightX = frameRect.left - containerLeft + frameRect.width - screenDelta;
 
         virtualImage.style.clipPath = `polygon(${leftX}px ${topY}px, ${rightX}px ${topY}px, ${rightX}px ${bottomY}px, ${leftX}px ${bottomY}px)`;
     }
