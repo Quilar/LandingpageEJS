807add21776b38bf04ebe0d49c06bedbc5378c4c
MomoQuilar <aajour@quilar.de>
2024-02-17 12:34:44 +0100

    SOLUTION-BENEFITS DESKTOP+LANDSCAPE

--- public/scripts/solution-benefits.js
+++ public/scripts/solution-benefits.js
@@ -1,29 +1,64 @@
-const initializeSwiperSolutionBenefits = () =>{
-  var swiper = new Swiper('.swiper-container-solution-benefits', {
+var swiperSolutionBenefit = null; // Holds the Swiper instance
+
+const initializeSwiperSolutionBenefit = () => {
+  console.log("Initialize Swiper");
+  swiperSolutionBenefit = new Swiper('.swiper-container-solution-benefits', {
+    // Swiper configuration
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
     effect: 'coverflow',
     grabCursor: true,
     centeredSlides: true,
     slidesPerView: 'auto',
     coverflowEffect: {
-      rotate: 50, // Degree of rotation between cards
-      stretch: 0, // Space between cards
-      depth: 100, // Depth effect size
-      modifier: 1, // Effect multiplier
-      slideShadows: true, // Enables the shadow effect
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
     },
-    loop: true, // Enables the infinite loop
+    loop: true,
     pagination: {
       el: '.swiper-pagination',
       clickable: true
     },
-    autoplay: {delay: 5000}
+    autoplay: { delay: 5000 }
   });
-}
 
-document.addEventListener('DOMContentLoaded', ()=>{
-  initializeSwiperSolutionBenefits();
-})
+  // Code for thumbnail click event handling
+  // Find all the thumbnails
+  var thumbnails = document.querySelectorAll('.video-thumbnail');
+
+  thumbnails.forEach(function(thumbnail) 
+  {
+      thumbnail.addEventListener('click', function() {
+        // Stop the autoplay when a thumbnail is clicked
+        swiperSolutionBenefit.autoplay.stop();
+      
+      });
+  });
+};
+
+const destroySwiperSolutionBenefit = () => {
+  if (swiperSolutionBenefit !== null) {
+    swiperSolutionBenefit.destroy();
+    swiperSolutionBenefit = null;
+    console.log("Swiper Destroyed");
+  }
+};
+
+const checkSwiperSolBenInitialization = () => {
+  if (window.innerWidth <= 992 && swiperSolutionBenefit === null) {
+    initializeSwiperSolutionBenefit();
+  } else if (window.innerWidth > 992 && swiperSolutionBenefit !== null) {
+    destroySwiperSolutionBenefit();
+  }
+};
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  checkSwiperSolBenInitialization(); // Check on initial load
+  window.addEventListener('resize', checkSwiperSolBenInitialization); // Check on resize
+});
