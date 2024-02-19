7fab27c1ffb8d61c9e94e53a9e48af61ca6baf02
MomoQuilar <aajour@quilar.de>
2024-02-19 09:32:12 +0100

    STYLES: Changed the navbar functionality. Made its position relative to stay at the top of the page. On mobile and landscape mode the navbar is the first time the website gets visited not visible to make the screen bigger for the hero background video

--- public/scripts/navbar.js
+++ public/scripts/navbar.js
@@ -6,13 +6,14 @@ function InitializeNavbar(){
     "scroll",
     () => {
         var { scrollY } = window;
-        if (scrollY - lastScrollTop > 0) {
-        // downward scroll
-        header.classList.remove("visible");
-        } else if (scrollY < lastScrollTop) {
-        // upward scroll
-        header.classList.add("visible");
-        } // else was horizontal scroll
+
+        if (scrollY < lastScrollTop) {
+            // vertical scroll has been detected
+            // If it was the first scroll make the navbar visible by now.
+            if(!header.classList.contains('visible'))
+                header.classList.add("visible");
+        } 
+        // else was horizontal scroll
         lastScrollTop = scrollY <= 0 ? 0 : scrollY;
         //Close the navbar box if it was open
         document.body.classList.remove('open');
@@ -21,4 +22,4 @@ function InitializeNavbar(){
     );
 }
 
-$(document).ready(()=>InitializeNavbar());
+$(document).ready(()=>InitializeNavbar()); 
