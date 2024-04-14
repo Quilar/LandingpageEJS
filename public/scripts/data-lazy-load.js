dec03af10e3190470feb4cb464cb5b9da816b957
MomoQuilar <aajour@quilar.de>
2024-04-14 18:06:53 +0200

    BUG-FIX: We had a problem with the video thumnails on mobile phones that they would not react to the click event to show the video. Problem was that the script was loading async and defer and the video thumbnails lazy, so we had no control at what time with element would be fully loaded in order to add an event. So we end up now, adding the lick event to the body to ensure that the click event definetly gets set because body will always be there before the script got loaded. And all clicks will be checked if a videothumnail got clicked to execute the planned feature.

--- public/scripts/data-lazy-load.js
+++ public/scripts/data-lazy-load.js
@@ -1,22 +1,20 @@
 //Code for the embedded videos on the website. Modifiying the iframe src property once clicked on the thumbnail. To reduce load-time
 
-document.addEventListener('DOMContentLoaded', function() 
-{
-    // Find all the thumbnails
-    var thumbnails = document.querySelectorAll('.video-thumbnail');
-    thumbnails.forEach(function(thumbnail) 
-    {
-        thumbnail.addEventListener('click', function() {
-        // The iframe is the previous sibling of the thumbnail image
-        var iframe = thumbnail.previousElementSibling;
-        // Get the video URL from the data-lazy-load attribute of the thumbnail
-        var videoUrl = iframe.getAttribute('data-lazy-load');
-        // Set the video URL as the src of the iframe and add autoplay
-        iframe.src = videoUrl + "&autoplay=1";
-    
-        // Show the iframe and hide the thumbnail
-        iframe.style.opacity = '1';
-        thumbnail.style.display = 'none';
-        });
+document.addEventListener('DOMContentLoaded', function() {
+    document.body.addEventListener('click', function(event) {
+        if (event.target.classList.contains('video-thumbnail')) {
+            var thumbnail = event.target;
+            var iframe = thumbnail.previousElementSibling;
+
+            var videoUrl = iframe.getAttribute('data-lazy-load');
+            if (videoUrl) {
+                iframe.src = videoUrl + "&autoplay=1";
+                iframe.style.opacity = '1';
+                thumbnail.style.display = 'none';
+            } else {
+                console.error('Data-lazy-load attribute is missing!');
+            }
+        }
     });
 });
+
