77bbf8cb56adcf1de25e2cc569b3b0e6f2c9f490
MomoQuilar <aajour@quilar.de>
2024-06-27 17:44:49 +0200

    NAVBAR: Made the navbar appearing at the initial load. No need to hide the navbar when first loaded

--- public/scripts/navbar.js
+++ public/scripts/navbar.js
@@ -2,7 +2,7 @@ function InitializeNavbar(){
     const header = document.querySelector("header");
     const navbar = document.querySelector("nav");
     let lastScrollTop = 0;
-    const scrollThreshold = 50;
+    const scrollThreshold = 100;
 
     window.addEventListener(
     "scroll",
@@ -12,19 +12,18 @@ function InitializeNavbar(){
         if (scrollY < lastScrollTop) {
             // vertical scroll has been detected
             // If it was the first scroll make the navbar visible by now.
-            if(!header.classList.contains('visible'))
-                navbar.classList.remove("init");
-                header.classList.add("visible");
+            if(header.classList.contains('invisible'))
+                header.classList.remove("invisible");
 
             if(lastScrollTop -scrollY > scrollThreshold){
-                header.classList.add("visible");
+                header.classList.remove("invisible");
             }
         } 
         else{
             //scrolling down
             if(lastScrollTop + scrollY > scrollThreshold){
                 //Prevent the snapback at the top remove the navbar
-                header.classList.remove("visible");
+                header.classList.add("invisible");
             }
         }
         
